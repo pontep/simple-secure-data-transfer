07649328769e36e637c299d23d44fae4d5e5d948
pontep <pontep.taveeshup@gmail.com>
2020-09-28 13:58:17 +0700

    add backend

---
+++ backend/src/main/java/com/pontep/simplesecurefullstack/SimpleSecureFullstackApplication.java
@@ -0,0 +1,13 @@
+package com.pontep.simplesecurefullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSecureFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSecureFullstackApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pontep/simplesecurefullstack/SimpleSecureFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontep.simplesecurefullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSecureFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
