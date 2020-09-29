ca67c62c97a1c841ffca1b30b55e8bc075d113e5
pontep <pontep.taveeshup@gmail.com>
2020-09-29 17:35:28 +0700

    reset

--- demo/src/main/java/com/example/demo/controller/VoteController.java
+++ demo/src/main/java/com/example/demo/controller/VoteController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import com.example.demo.repository.VoteRepository;
-import org.apache.tomcat.util.net.openssl.ciphers.Encryption;
+import com.rockaport.alice.Alice;
+import com.rockaport.alice.AliceContext;
+import com.rockaport.alice.AliceContextBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.GeneralSecurityException;
 import java.util.Objects;
 
 @CrossOrigin(origins = "*")
@@ -24,8 +27,18 @@ public class VoteController {
     @PostMapping("/vote")
     public ResponseEntity<?> newVote(@RequestBody String encryptedBody){
         System.out.println("Received: "+encryptedBody.toString());
-
-
+        AliceContext aliceContext = new AliceContextBuilder()
+                .setAlgorithm(AliceContext.Algorithm.AES)
+                .setMode(AliceContext.Mode.CBC) // or AliceContext.Mode.CTR
+                .setIvLength(16)
+                .build();
+        Alice alice = new Alice(aliceContext);
+        try{
+            String decryptedBytes = alice.decrypt(encryptedBody.getBytes(), "pontep1234".toCharArray()).toString();
+            System.out.println("Decrypted: " + decryptedBytes);
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
         return ResponseEntity.ok().body("Vote successfully.");
     }
 }
